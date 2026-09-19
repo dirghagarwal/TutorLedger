f4a27a81c19517fca2dc7e3ac86a969ac3fd4131
DIRGH AGARWAL <74349502+dirghagarwal@users.noreply.github.com>
2026-09-19 23:51:14 +0530

    fix: destructure error in route boundary

--- app/error.tsx
+++ app/error.tsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import { Button } from "@/components/ui/button";
 
 export default function Error({
+  error,
   reset,
 }: Readonly<{
   error: Error & { digest?: string };
